e33aa35b6c71147f2d5107133b3dee39eccc6a67
PankajKumar Yadav <132429460+PankajYadav67@users.noreply.github.com>
2023-12-14 12:56:07 +0530

    1.3.01 Minor Fix

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import BigHero1 from "../Assets/BigHero/BigHero2.png";
 import BigHero2 from "../Assets/BigHero/bigHero.png";
 import Banner from "../Assets/BigHero/paan-corner-banner-desktop.png";
 import { ProductCategory } from "../Product Category/ProductCategory";
-import { BankDiscountCards} from "../BankDiscount/BankDiscount";
+
 import { FetchBankOffer} from "../BankDiscount/BankFetch";
 
 export const HeroSection = () => {
@@ -22,7 +22,7 @@ export const HeroSection = () => {
       </div>
 
          <FetchBankOffer/>  
-      {/* <BankDiscountCards/> */}
+
 
       <div className="">
         <a href="/" >
